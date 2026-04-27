e720cab4fcfa44c8d65d78658fb7dc19fd8ae81b
ALEXGNH <nolasco.hdz.anagabriela.c.2021@gmail.com>
2026-04-26 22:12:43 -0600

    animation and transition

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
  const mybutton = document.getElementById('playaction');
         const home = document.getElementById('home')
         const game1 = document.getElementById('game1')
+
         mybutton.addEventListener('click', () => {
             home.style.display = 'none'
             game1.style.display = 'flex';
