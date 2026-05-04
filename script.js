36624bd81b17483f97d600e78f89f9bd3497a4f6
ALEXGNH <nolasco.hdz.anagabriela.c.2021@gmail.com>
2026-05-03 23:40:05 -0600

    i did everything again
    this time everything is okay and works right, plsssss count this in my hours, i put so much effort trying to understand thiiisss

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const game1button = document.getElementById('buttonchangepage1');
+const game1button = document.getElementById ('buttonchangepage1');
         const homepage = document.getElementById('homepage')
         const game1 = document.getElementById('game1')
 
@@ -10,11 +10,22 @@ const game1button = document.getElementById('buttonchangepage1');
         });
 
 function back() {
-    document.getElementById('homepage').style.display = 'block'
-    document.getElementById('game1').style.display = 'none'
 
-     game1button.addEventListener('click', () => {
-            game1.style.display = 'none';
-            homepage.style.display = 'flex'
+       document.getElementById('game1').style.display = 'none'
+       document.getElementById('homepage').style.display = 'block'
+
+       game1button.addEventListener('click', () => {
+              game1.style.display = 'none';
+              homepage.style.display = 'block'
         });
-}       
+
+        const game1button = document.getElementById ('buttonchangepage1');
+        const homepage = document.getElementById('homepage')
+        const game1 = document.getElementById('game1')
+
+        game1button.addEventListener('click', () => {
+            homepage.style.display = 'none'
+            game1.style.display = 'flex';
+        });
+}      
+
