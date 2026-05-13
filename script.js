820c4488f04c1e5f918dff836058c9ca12c51104
ALEXGNH <nolasco.hdz.anagabriela.c.2021@gmail.com>
2026-05-12 23:22:51 -0600

    Cleaning and cursor follower

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+const cursor = document.querySelector('.custom-cursor');
+      document.addEventListener('mousemove', (e) =>
+      {
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+
+      });
+
 
 const game1button = document.getElementById ('buttonchangepage1');
         const homepage = document.getElementById('homepage')
